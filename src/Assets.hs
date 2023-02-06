b8e917452dab5dec3fe42cd93650f12826fa3e88
Simon Plakolb <s.plakolb@gmail.com>
2023-02-06 23:26:12 +0100

    css: Use built in compression

--- src/Assets.hs
+++ src/Assets.hs
@@ -2,14 +2,14 @@
 
 module Assets where
 
-import Hakyll (Pattern, Rules, compile, copyFileCompiler, idRoute, match, route)
+import Hakyll (Pattern, Rules, compile, copyFileCompiler, idRoute, match, route, compressCssCompiler)
 
 copy :: Pattern -> Rules ()
 copy p = match p (route idRoute >> compile copyFileCompiler)
 
 copyAssets = do
   copy "assets/images/*"
-  copy "assets/css/**"
+  match "assets/css/**" (route idRoute >> compile compressCssCompiler)
   copy "assets/js/*"
   copy "assets/fonts/*"
   copy "assets/webfonts/*"
