70567faaab3fdcd16808df7b19dde4cdaa18995e
Simon Plakolb <s.plakolb@gmail.com>
2023-02-06 22:39:19 +0100

    site: Update fixes

--- src/Assets.hs
+++ src/Assets.hs
@@ -9,8 +9,8 @@ copy p = match p (route idRoute >> compile copyFileCompiler)
 
 copyAssets = do
   copy "assets/images/*"
-  copy "assets/css/*"
-  copy "assets/js/**"
+  copy "assets/css/**"
+  copy "assets/js/*"
   copy "assets/fonts/*"
   copy "assets/webfonts/*"
   copy "CNAME"
